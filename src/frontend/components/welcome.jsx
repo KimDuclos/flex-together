36910908a428478a9f35e5830a2aa63ecb13a9bc
KimDuclos <kimduclos@gmail.com>
2020-07-12 11:50:36 -0600

    loaded css and image on landing page

--- src/frontend/components/welcome.jsx
+++ src/frontend/components/welcome.jsx
@@ -1,15 +1,17 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./welcome.css";
 
 const Welcome = () => {
-	return (
-		<div className="welcome">
-			<h1>Welcome to FlexTogether</h1>
-			<button className="startBtn">
-				<Link to="/whoAreYou">Start</Link>
-			</button>
-		</div>
-	);
+  return (
+    <div className="welcome">
+      <img className="landingImage" src="./images/landingImage.png" alt="man working out" />
+      <h2>Welcome to FlexTogether</h2>
+      <button className="startBtn">
+        <Link to="/whoAreYou">Start</Link>
+      </button>
+    </div>
+  );
 };
 
 export default Welcome;
