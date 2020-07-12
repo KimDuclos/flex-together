551d6dbf36268fb63cebc16b1b5283f93c81f159
KimDuclos <kimduclos@gmail.com>
2020-07-12 14:25:24 -0600

    landing page responsive to 1000px

--- src/frontend/components/welcome.jsx
+++ src/frontend/components/welcome.jsx
@@ -1,17 +1,17 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "./welcome.css";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './welcome.scss';
 
 const Welcome = () => {
-  return (
-    <div className="welcome">
-      <img className="landingImage" src="./images/landingImage.png" alt="man working out" />
-      <h2>Welcome to FlexTogether</h2>
-      <button className="startBtn">
-        <Link to="/whoAreYou">Start</Link>
-      </button>
-    </div>
-  );
+	return (
+		<div className="welcome">
+			<img className="landingImage" src="./images/landingImage.png" alt="man working out"/>
+				<div className="title">Welcome to FlexTogether</div>
+				<button className="startBtn">
+					<Link to="/whoAreYou">Start</Link>
+				</button>
+		</div>
+	);
 };
 
 export default Welcome;
